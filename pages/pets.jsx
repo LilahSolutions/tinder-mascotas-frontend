8a2adbaafe5fb72dad20836135261e9d65a013e5
Rocío Mena <rociocmena@gmail.com>
2022-03-15 00:45:57 -0300

    Implement advances on pets screen

--- pages/pets.jsx
+++ pages/pets.jsx
@@ -1,10 +1,89 @@
+import Link from 'next/link';
+// import {withAuth, usePets} from '../utils/auth';
+import Button from '../components/Button';
 import {PetsTabNavigator} from '../components/TabNavigator';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Pets.module.css';
 
-function Home() {
-	return <main className={styles.container}>Mascotas</main>;
-}
+const Pets = () => {
+	// const pets = usePets();
+	const pets = [
+		{
+			id: 1,
+			name: 'Jaskier',
+			gender: 'male',
+			type: 'cat',
+			photo: '/assets/cat-pic.jpg',
+		},
+		{
+			id: 2,
+			name: 'Jaskier',
+			gender: 'male',
+			type: 'cat',
+			photo: '/assets/cat-pic.jpg',
+		},
+		{
+			id: 3,
+			name: 'Jaskier',
+			gender: 'male',
+			type: 'cat',
+			photo: '/assets/cat-pic.jpg',
+		},
+	];
 
-Home.TabNavigator = PetsTabNavigator;
+	const deletePet = (id) => {
+		console.log('Goodbye, ', id);
+	};
 
-export default Home;
+	return (
+		<main className={styles.container}>
+			<h2 className="box">Mis mascotas</h2>
+			<Link href="/pet/add" passHref>
+				<Button
+					label="Agregar mascota"
+					size="small"
+					className={styles.addButton}
+				/>
+			</Link>
+			<ul className={styles.petsList}>
+				{pets.map((pet) => (
+					<li key={pet.id} className={styles.petItem}>
+						<img
+							className={styles.photo}
+							src={pet.photo}
+							alt="Foto de mascota"
+						/>
+						<div>
+							<h4>
+								Nombre: <span>{pet.name}</span>
+							</h4>
+							<h4>
+								Sexo: <span>{pet.gender}</span>
+							</h4>
+							<h4>
+								Especie: <span>{pet.type}</span>
+							</h4>
+						</div>
+						<div>
+							<Link href={`/pets/${pet.id}`} passHref>
+								<Button label="Ver" size="small" />
+							</Link>
+							<Link href="/pet/edit" passHref>
+								<Button label="Modificar" size="small" />
+							</Link>
+							<Button
+								label="Eliminar"
+								size="small"
+								handleClick={() => deletePet(pet.id)}
+							/>
+						</div>
+					</li>
+				))}
+			</ul>
+		</main>
+	);
+};
+
+Pets.TabNavigator = PetsTabNavigator;
+
+export default Pets;
+// export default withAuth(Pets);
