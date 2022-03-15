2aadf2075fe4c56171ec4cd84b17f88d6712305f
Rocío Mena <rociocmena@gmail.com>
2022-03-14 22:15:05 -0300

    Implement user page

--- pages/user.jsx
+++ pages/user.jsx
@@ -1,10 +1,33 @@
 import {ProfileTabNavigator} from '../components/TabNavigator';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/User.module.css';
 
-function Home() {
-	return <main className={styles.container}>Mi perfil</main>;
+function Profile() {
+	const user = {
+		name: 'Ro',
+		lastname: 'Mena',
+		email: 'r@lila.com',
+		photo: '/assets/cat-pic.jpg',
+	};
+
+	return (
+		<main className={styles.container}>
+			<h2 className="box">Mi perfil</h2>
+			<img className={styles.photo} src={user.photo} alt="Foto de perfil" />
+			<div className={styles.dataContainer}>
+				<h4>
+					Nombre: <span>{user.name}</span>
+				</h4>
+				<h4>
+					Apellido: <span>{user.lastname}</span>
+				</h4>
+				<h4>
+					Correo electrónico: <span>{user.email}</span>
+				</h4>
+			</div>
+		</main>
+	);
 }
 
-Home.TabNavigator = ProfileTabNavigator;
+Profile.TabNavigator = ProfileTabNavigator;
 
-export default Home;
+export default Profile;
