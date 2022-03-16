2558e0d71745c641d962aea53097ce42835478dc
Rocío Mena <69587750+RocioCM@users.noreply.github.com>
2022-03-15 21:27:37 -0300

    Implement profile and pets screen (#9)
    * Implement user page
    
    * Implement pets page
    
    * Mock user in context to integrate withAuth

--- pages/pets.jsx
+++ pages/pets.jsx
@@ -1,10 +1,64 @@
+import Link from 'next/link';
+import {withAuth, usePets} from '../utils/auth';
+import Button from '../components/Button';
 import {PetsTabNavigator} from '../components/TabNavigator';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Pets.module.css';
 
-function Home() {
-	return <main className={styles.container}>Mascotas</main>;
-}
+const Pets = () => {
+	const pets = usePets();
 
-Home.TabNavigator = PetsTabNavigator;
+	const deletePet = (id) => {
+		console.log('Goodbye, ', id);
+	};
 
-export default Home;
+	return (
+		<main className={styles.container}>
+			<h2 className="box">Mis mascotas</h2>
+			<ul className={styles.petsList}>
+				<li className={styles.addButton}>
+					<Link href="/pet/add" passHref>
+						<Button label="Agregar mascota" size="small" />
+					</Link>
+				</li>
+				{pets.map((pet) => (
+					<li key={pet.id} className={styles.petItem}>
+						<img
+							className={styles.photo}
+							src={pet.photo}
+							alt="Foto de mascota"
+						/>
+						<div className={styles.dataContainer}>
+							<h4>
+								Nombre: <span>{pet.name}</span>
+							</h4>
+							<h4>
+								Sexo: <span>{pet.gender}</span>
+							</h4>
+							<h4>
+								Especie: <span>{pet.type}</span>
+							</h4>
+						</div>
+						<div className={styles.buttonsContainer}>
+							<Link href={`/pets/${pet.id}`} passHref>
+								<Button label="Ver" size="small" />
+							</Link>
+							<Link href="/pet/edit" passHref>
+								<Button label="Modificar" size="small" />
+							</Link>
+							<Button
+								label="Eliminar"
+								size="small"
+								handleClick={() => deletePet(pet.id)}
+							/>
+						</div>
+					</li>
+				))}
+			</ul>
+		</main>
+	);
+};
+
+const Component = withAuth(Pets);
+Component.TabNavigator = PetsTabNavigator;
+
+export default Component;
--- utils/auth/withAuth.jsx
+++ utils/auth/withAuth.jsx
@@ -3,29 +3,29 @@ import {useRouter} from 'next/router';
 import {useLoginContext} from './context';
 import {AuthServices} from '../../services/AuthServices';
 
-// eslint-disable-next-line react/display-name
-const withAuth = (Component) => (props) => {
-	const {isLoggedIn, dispatch} = useLoginContext();
-	const [loading, setLoading] = useState(true);
-	const router = useRouter();
+const withAuth = (Component) =>
+	function AuthWrapper(props) {
+		const {isLoggedIn, dispatch} = useLoginContext();
+		const [loading, setLoading] = useState(true);
+		const router = useRouter();
 
-	useEffect(() => {
-		const fetchUser = async (token) => {
-			const {user, status} = await AuthServices.getUserByToken(token);
-			if (status === 'success') dispatch({type: 'login', value: user});
-			setLoading(false);
-		};
-		const sessionToken = sessionStorage.getItem('sessionToken');
-		if (sessionToken) fetchUser(sessionToken);
-		else setLoading(false);
-	}, []);
+		useEffect(() => {
+			const fetchUser = async (token) => {
+				const {user, status} = await AuthServices.getUserByToken(token);
+				if (status === 'success') dispatch({type: 'login', value: user});
+				setLoading(false);
+			};
+			const sessionToken = sessionStorage.getItem('sessionToken');
+			if (sessionToken) fetchUser(sessionToken);
+			else setLoading(false);
+		}, []);
 
-	useEffect(() => {
-		// Loading is true while we are trying to fetch the user from BE (if sessionToken was found on sessionStorage).
-		if (!loading && !isLoggedIn) router.push('/home');
-	}, [loading]);
+		useEffect(() => {
+			// Loading is true while we are trying to fetch the user from BE (if sessionToken was found on sessionStorage).
+			if (!loading && !isLoggedIn) router.push('/home');
+		}, [loading]);
 
-	return isLoggedIn && !loading && <Component {...props} />;
-};
+		return isLoggedIn && !loading && <Component {...props} />;
+	};
 
 export default withAuth;
