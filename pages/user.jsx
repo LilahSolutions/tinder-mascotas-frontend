a98a87cc427f14e0fa0b3705069847ce6755db7b
Rocío Mena <rociocmena@gmail.com>
2022-03-23 19:22:06 -0300

    Update user screen

--- pages/user.jsx
+++ pages/user.jsx
@@ -1,14 +1,15 @@
-import {withAuth, useUser} from '../utils/auth';
+import Link from 'next/link';
+import {withAuth, useLoginContext} from '../utils/auth';
 import {ProfileTabNavigator} from '../components/TabNavigator';
+import Button from '../components/Button';
 import styles from '../styles/User.module.css';
 
 function Profile() {
-	const user = useUser();
+	const {user, pets} = useLoginContext();
 
 	return (
 		<main className={styles.container}>
 			<h2 className="box">Mi perfil</h2>
-			<img className={styles.photo} src={user.image} alt="Foto de perfil" />
 			<div className={styles.dataContainer}>
 				<h4>
 					Nombre: <span>{user.name}</span>
@@ -19,7 +20,13 @@ function Profile() {
 				<h4>
 					Correo electrónico: <span>{user.email}</span>
 				</h4>
+				<h4>
+					Cantidad de mascotas: <span>{pets.length}</span>
+				</h4>
 			</div>
+			<Link href={`/home`} passHref>
+				<Button label="Cerrar sesión" size="large" />
+			</Link>
 		</main>
 	);
 }
