91bcc3054732cbc11f61dcb22a59b66958303f01
Rocío Mena <rociocmena@gmail.com>
2022-03-24 22:05:35 -0300

    Remove label

--- components/MatchButton/MatchButton.jsx
+++ components/MatchButton/MatchButton.jsx
@@ -1,12 +1,15 @@
-import styles from './MatchButton.module.css'
+import styles from './MatchButton.module.css';
 
 const MatchButton = ({handler}) => {
-	return(
+	return (
 		<div className={styles.buttonContainer} onClick={() => handler()}>
-			<img className={styles.buttonImage} src='/assets/match-img.png' alt='match button' />
-			<span className={styles.buttonText}>¡Me gusta!</span>
+			<img
+				className={styles.buttonImage}
+				src="/assets/match-img.png"
+				alt="match button"
+			/>
 		</div>
-	)
-}
+	);
+};
 
 export default MatchButton;
